d09ef898b274a6e2ad15ac48db7d410cd5fbb074
saddalmatian <saddalmatian@gmail.com>
2021-11-13 11:42:42 +0700

    Delete unused folder

--- app/app/models/schemas/user.py
+++
@@ -1,8 +0,0 @@
-from ..domains import user
-
-
-# class UserIn(user.User):
-#     pass 
-
-class UserResp(user.User):
-    pass
--- app/app/services/user.py
+++
@@ -1,7 +0,0 @@
-from app.db.users.get_user import get_user
-
-
-class UserService:
-    def get_user(request):
-        response = get_user(request)
-        return response 

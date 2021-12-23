5afbd6572c83cd66e8ff621ea66dd8d24891d7a8
saddalmatian <saddalmatian@gmail.com>
2021-12-23 13:47:39 +0700

    add bucket.create()

--- s3/bucket.py
+++ s3/bucket.py
@@ -9,9 +9,22 @@ bucket = resource.Bucket('gianghoatran')
 def print_json(response :str):
     print(json.dumps(response, indent=4, sort_keys=True, default=str))
 
+# Create a bucket
+# response = bucket.create(
+#     CreateBucketConfiguration={
+#         'LocationConstraint': 'ap-southeast-1',
+#     },
+# )
+# print_json(response)
+
 
 # Upload an object 
 # bucket.upload_file('text.txt', 'hello.txt')
+
+
+# List objects
+# for obj in bucket.objects.all():
+#     print(obj.key)
 
 
 # Delete an object

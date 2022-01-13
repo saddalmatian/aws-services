3390c93a9d2b9423ae207812139c067708504606
saddalmatian <59312063+saddalmatian@users.noreply.github.com>
2022-01-13 14:18:47 +0700

    Update invoke.py

--- lambda/invoke.py
+++ lambda/invoke.py
@@ -2,11 +2,13 @@ import boto3,json
 
 client=boto3.client('lambda')
 
+
+# This is test event when you invoke
 test_event={
     "key1":"PhantomAL"
 }
 
-
+# Use to invoke a lambda function
 response = client.invoke(
     FunctionName='helloWorldLambda',
     Payload=json.dumps(test_event),
